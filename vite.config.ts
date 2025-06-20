587323824ba0b0a512455792aa70c67cdbf314a4
UtibeAbasi Akpan <106952875+EpicShadow1@users.noreply.github.com>
2025-06-20 12:53:09 +0100

    {commit_message}

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/UtibeProtfolio/', // 👈 Add this line (repo name with slashes)
   plugins: [react()],
 })
